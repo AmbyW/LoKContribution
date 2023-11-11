6242ee10f277a7cad2fdb969feb3e0c0224fadc3
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-11-11 21:42:51 +0700

    modifcada la funcion de generar las url para que use una nueva funcion que si el espacio de tiempo es mayor de una semana haga 2 peticiones en lugar de una sola con todo el rango de fechas, ya que si se utiliza un rango de fechas myor de una semana el servidor del api de devuelve un error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import datetime
+import time
 import requests
 import json
 from flask import Flask, render_template, request, jsonify
@@ -9,29 +11,30 @@ from flask import Flask, render_template, request, jsonify
 LOK_MAP = [i for i in range(100000, 165536)]
 
 
-def get_adyacent_lands(land_number):
+def get_adyacent_lands(land_number, adjacent_lands):
     try:
         land_number = int(land_number)
     except ValueError:
         pass
     if 100000 <= land_number <= 165535:
         prev_land = up_prev_land = down_prev_land = next_land = up_next_land = down_next_land = up_land = down_land = 0
-        if (land_number - 100000) % 256 != 0:
-            prev_land = land_number - 1
-            if land_number > 100255:
-                down_prev_land = land_number - 257
+        if adjacent_lands:
+            if (land_number - 100000) % 256 != 0:
+                prev_land = land_number - 1
+                if land_number > 100255:
+                    down_prev_land = land_number - 257
+                if land_number < 165280:
+                    up_prev_land = land_number + 255
+            if (land_number - 99999) % 256 != 0:
+                next_land = land_number + 1
+                if land_number < 165280:
+                    up_next_land = land_number + 257
+                if land_number > 100255:
+                    down_next_land = land_number - 255
             if land_number < 165280:
-                up_prev_land = land_number + 255
-        if (land_number - 99999) % 256 != 0:
-            next_land = land_number + 1
-            if land_number < 165280:
-                up_next_land = land_number + 257
+                up_land = land_number + 256
             if land_number > 100255:
-                down_next_land = land_number - 255
-        if land_number < 165280:
-            up_land = land_number + 256
-        if land_number > 100255:
-            down_land = land_number - 256
+                down_land = land_number - 256
         return [
                 {"position": 1, "id": up_prev_land},
                 {"position": 2, "id": up_land},
@@ -45,20 +48,41 @@ def get_adyacent_lands(land_number):
         ]
 
 
-def make_urls(land_number, from_date, to_date):
+def get_date_ranges(from_date, to_date):
+    date_starts = datetime.datetime.strptime(from_date, '%Y-%m-%d')
+    date_ends = datetime.datetime.strptime(to_date, '%Y-%m-%d')
+    date_range = abs(date_starts - date_ends)
+    if date_range.days < 7:
+        return [(from_date, to_date),]
+    range_date_days = date_range.days
+    date_list = []
+    while range_date_days > 0:
+        next_days = 6 if range_date_days >= 7 else range_date_days
+        range_date_days -= next_days
+        date_list.append((date_starts.strftime('%Y-%m-%d'),
+                          (date_starts + datetime.timedelta(days=next_days - 1)).strftime('%Y-%m-%d')))
+        date_starts += datetime.timedelta(days=next_days)
+    return date_list
+
+
+def make_urls(land_number, from_date, to_date, adjacent_lands=False):
     urls = []
-    for land in get_adyacent_lands(land_number):
-        if land["id"] >= 100000:
-            urls.append({"id": land["id"],
-                         "position": land["position"],
-                         "url": f"https://api-lok-live.leagueofkingdoms.com/api/stat/land/contribution?landId={land['id']}&from={from_date}&to={to_date}"})
+    for land in get_adyacent_lands(land_number, adjacent_lands):
+        for date_start, date_end in get_date_ranges(from_date, to_date):
+            if land["id"] >= 100000:
+                urls.append({"id": land["id"],
+                             "position": land["position"],
+                             "url": f"https://api-lok-live.leagueofkingdoms.com/api/stat/land/contribution?landId={land['id']}&from={date_start}&to={date_end}"})
     return urls
 
 
 def get_lands_data(urls):
     responses = []
     for url in urls:
+        time2 = time.time()
+        print('requesting url: ', url.get('url', ''))
         response = requests.get(url.get('url', ''))
+        print('response in: ', time.time() - time2)
         if response.ok:
             content = json.loads(response.content.decode('utf-8'))
             if content.get('result', False):
@@ -79,7 +103,7 @@ def process_lands_data(responses, urls, kingdom_name):
     owners = []
     for data in responses:
         exists_owner = False
-        wallet = data.get('land_id', 0)
+        wallet = data.get('owner', '0x0000000000')
         land = {
             'land': data.get('land_id', 0),
             'owner': wallet,
@@ -110,7 +134,8 @@ def get_contribution():
     land = request.args.get('land_id')
     from_date = request.args.get('from')
     to_date = request.args.get('to')
-    urls = make_urls(land, from_date, to_date)
+    adjacent_lands = True if request.args.get('adjacent_lands') == 'true' else False
+    urls = make_urls(land, from_date, to_date, adjacent_lands)
     responses = get_lands_data(urls)
     response = process_lands_data(responses, urls, kingdom)
     return jsonify(response), 200
@@ -123,7 +148,7 @@ def index():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=5000)
+    flask_app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
